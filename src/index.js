0129b60e3881ba017756c96af56ad2576920b838
Giorgio <20621396+giorgi-o@users.noreply.github.com>
2022-09-27 22:39:34 +0100

    add fetching search suggestions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import {getToken, setToken, fetchToken} from "./auth.js";
 import {fetchMetadata, fetchSong, fetchAlbum, fetchArtist, fetchPlaylist, fetchBulkMetadata, fetchIsrc, fetchUpc} from "./metadata.js";
-import {search} from "./search.js";
+import {search, suggestions} from "./search.js";
 import {doRequest, formatArtworkUrl} from "./util.js";
 
 export {
     getToken, setToken, fetchToken,
     fetchMetadata, fetchSong, fetchAlbum, fetchArtist, fetchPlaylist, fetchBulkMetadata, fetchIsrc, fetchUpc,
-    search,
+    search, suggestions,
     doRequest, formatArtworkUrl
 }
